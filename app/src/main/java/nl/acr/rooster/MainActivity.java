4a36ec3429f296aec3e6aae1f633eed8caccc1b0
Dreaded_X <tim.huizinga@gmail.com>
2015-11-21 01:55:25 +0100

    Inlog systeem op basis van ACR-framework gemaakt

--- app/src/main/java/nl/acr/rooster/MainActivity.java
+++ app/src/main/java/nl/acr/rooster/MainActivity.java
@@ -1,13 +1,22 @@
 package nl.acr.rooster;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import go.framework.Framework;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +30,57 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getToken();
+            }
+        });
+
+        EditText code = (EditText)findViewById(R.id.code);
+        code.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    getToken();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+    }
+
+    // TODO: This needs to be handled better
+    public void getToken() {
+        // TODO: Figure out how to disable the login button after it has been pressed to prevent spamming
+        EditText school = (EditText)findViewById(R.id.school);
+        EditText code = (EditText)findViewById(R.id.code);
+
+        Log.w("School", school.getText().toString());
+        Log.w("Code", code.getText().toString());
+
+        String token = Framework.GetToken(school.getText().toString(), code.getText().toString());
+
+        TextView textView = (TextView) findViewById(R.id.codeView);
+        if (token != null) {
+            Log.w("Token", token);
+
+            // NOTE: This is just for testing
+            textView.setText("Acces token: " + token);
+        } else {
+
+            // NOTE: This is just for testing
+            textView.setText("Invalid login");
+        }
     }
 
     @Override
