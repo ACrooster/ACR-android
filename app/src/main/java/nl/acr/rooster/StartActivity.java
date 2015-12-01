d39bf9a55cfdef9aecf47a985dccfdf0df5b7fba
Dreaded_X <tim.huizinga@gmail.com>
2015-12-01 18:49:52 +0100

    Ghetto update systeem ingebouwd

--- app/src/main/java/nl/acr/rooster/StartActivity.java
+++ app/src/main/java/nl/acr/rooster/StartActivity.java
@@ -2,9 +2,23 @@ package nl.acr.rooster;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
@@ -20,6 +34,10 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // NOTE: This function is only here while we are not in the Play Store
+        UpdateTask updateTask = new UpdateTask();
+        updateTask.execute("https://www.dropbox.com/s/tue3bcvot2ou5z6/app-debug.apk?raw=1");
 
         // All this activity does is check if a token already exists
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
@@ -47,4 +65,78 @@ public class StartActivity extends AppCompatActivity {
 
     }
 
+    public class UpdateTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            URLConnection feedUrl;
+            try {
+                feedUrl = new URL("https://www.dropbox.com/s/pybz6xih5piop68/version?raw=1").openConnection();
+                InputStream is = feedUrl.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "");
+                }
+                is.close();
+
+                PackageInfo packageInfo = getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
+                int curVersionCode = packageInfo.versionCode;
+                int newVersionCode = Integer.parseInt(sb.toString());
+
+                Log.w("Update", "Current version: " + curVersionCode + ", Newest version: " + newVersionCode);
+                if (newVersionCode > curVersionCode) {
+                    String path = Environment.getExternalStorageDirectory().getPath() + "/acrooster.apk";
+                    try {
+                        URL url = new URL(sUrl[0]);
+                        URLConnection connection = url.openConnection();
+                        connection.connect();
+
+                        int fileLength = connection.getContentLength();
+
+                        // download the file
+                        InputStream input = new BufferedInputStream(url.openStream());
+                        OutputStream output = new FileOutputStream(path);
+
+                        byte data[] = new byte[1024];
+                        long total = 0;
+                        int count;
+                        while ((count = input.read(data)) != -1) {
+                            total += count;
+                            publishProgress((int) (total * 100 / fileLength));
+                            output.write(data, 0, count);
+                        }
+
+                        output.flush();
+                        output.close();
+                        input.close();
+
+                        return path;
+                    } catch (Exception e) {
+                        Log.e("YourApp", "Well that didn't work out so well...");
+                        Log.e("YourApp", e.getMessage());
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("Get Url", "Error in downloading: " + e.toString());
+            }
+
+            return "";
+        }
+
+        // begin the installation by opening the resulting file
+        @Override
+        protected void onPostExecute(String path) {
+            if (!path.equals("")) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setDataAndType(Uri.fromFile(new File(path)), "application/vnd.android.package-archive");
+                Log.d("Lofting", "About to install new .apk");
+                startActivity(i);
+            }
+        }
+    }
 }
--- copy2dropbox.sh
+++ copy2dropbox.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cp ./app/build/outputs/apk/app-debug.apk ~/Documenten/Dropbox/School/Informatica/Rooster/
+git rev-list --all --count > ~/Documenten/Dropbox/School/Informatica/Rooster/version
