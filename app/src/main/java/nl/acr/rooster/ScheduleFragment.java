20baa21e892c7e9b6ee51dffdce5423c4b1d0d83
Tim Huizinga <DreadedX@users.noreply.github.com>
2016-01-08 11:25:35 +0100

    getDay en getMonth functie versimpeld (Need testing)

--- app/src/main/java/nl/acr/rooster/ScheduleFragment.java
+++ app/src/main/java/nl/acr/rooster/ScheduleFragment.java
@@ -119,54 +119,18 @@ public class ScheduleFragment extends Fragment {
 
     // TODO: Check if there is a built in system get this
     public static String getDay(int index) {
-
-        switch (index) {
-            case 0:
-                return days[0];
-            case 1:
-                return days[1];
-            case 2:
-                return days[2];
-            case 3:
-                return days[3];
-            case 4:
-                return days[4];
-        }
-
-        return "";
+	    if (index > 0 && index <= days.length) {
+		    return days[i-1];
+	    }
+	    return "";
     }
 
     // TODO: Check if there is a built in system get this
     public static String getMonth(int index) {
-
-        switch (index) {
-            case 1:
-                return months[0];
-            case 2:
-                return months[1];
-            case 3:
-                return months[2];
-            case 4:
-                return months[3];
-            case 5:
-                return months[4];
-            case 6:
-                return months[5];
-            case 7:
-                return months[6];
-            case 8:
-                return months[7];
-            case 9:
-                return months[8];
-            case 10:
-                return months[9];
-            case 11:
-                return months[10];
-            case 12:
-                return months[11];
-        }
-
-        return "";
+    	if (index > 0 && index <= months.length) {
+		    return months[i-1];
+	    }
+	    return "";
     }
 }
 
